79a6900b4ea91631e976ae7cf71463d5d176ddf7
FabriSilve <fabrizio.silvestri@yahoo.it>
2017-11-10 12:03:31 +0100

    Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,12 @@ import { Hero } from '../hero';
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  selectedHero : Hero;
 
+  onSelect(hero : Hero) : void {
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
