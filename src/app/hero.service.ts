86f4eb7bc454282f153fe391de88a7f4065cadbb
FabriSilve <fabrizio.silvestri@yahoo.it>
2017-11-17 12:07:37 +0100

    update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,9 @@ import { MessageService } from './message.service';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';  // URL to web api
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }; 
 
   constructor(
     private http : HttpClient,
@@ -57,5 +60,12 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  updateHero(hero : Hero) :Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
