5ecf16a15fc99df9fdb19114a817c2a9e63394f5
FabriSilve <fabrizio.silvestri@yahoo.it>
2017-11-17 12:18:15 +0100

    Add Hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,4 +68,12 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+
+  addHero(hero : Hero) : Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
 }
